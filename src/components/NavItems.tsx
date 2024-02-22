941bcd9b72977bf826827a87780703a5ceb21cda
Dmitriy Mazurenko <dimazurenko@gmail.com>
2024-02-22 13:01:14 +0300

    add useOnClickOutside hook

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,16 +1,36 @@
 'use client';
 
-import {useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {PRODUCT_CATEGORIES} from '@/config';
 import NavItem from '@/components/NavItem';
+import {useOnClickOutside} from '@/hooks/use-on-click-outside';
+import {handler} from 'tailwindcss-animate';
 
 const NavItems = () => {
 	const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
+	useEffect(() => {
+		const handler = (e: KeyboardEvent) => {
+			if (e.key === 'Escape') {
+				setActiveIndex(null);
+			}
+		};
+
+		document.addEventListener('keydown', handler);
+
+		return () => {
+			document.removeEventListener('keydown', handler);
+		};
+	}, []);
+
 	const isAnyOpen = activeIndex !== null;
 
+	const navRef = useRef<HTMLDivElement | null>(null);
+
+	useOnClickOutside(navRef, () => setActiveIndex(null));
+
 	return (
-		<div className="flex gap-4 h-full">
+		<div className="flex gap-4 h-full" ref={navRef}>
 			{PRODUCT_CATEGORIES.map((category, i) => {
 				const handleOpen = () => {
 					if (activeIndex === i) {
