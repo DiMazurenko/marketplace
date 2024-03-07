61969ae67820d028cfe0d37f9915e3aef4cde5c7
Dmitriy Mazurenko <dimazurenko@gmail.com>
2024-03-07 12:59:56 +0300

    update verify-email page

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import VerifyEmail from '@/components/VerifyEmail';
 
 interface PageProps {
 	searchParams: {
@@ -7,15 +8,17 @@ interface PageProps {
 	};
 }
 
-const VerifyEmailPage = ({searchParams}: PageProps) => {
+const VerifyEmailPage = ({ searchParams }: PageProps) => {
 	const token = searchParams.token;
 	const toEmail = searchParams.to;
 
 	return (
 		<div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
 			<div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-				{token && typeof token === 'string' ? (
-					<div className="grid gap-6"></div>
+				{ token && typeof token === 'string' ? (
+					<div className="grid gap-6">
+						<VerifyEmail token={ token }/>
+					</div>
 				) : (
 					<div className="flex h-full flex-col items-center justify-center space-y-1">
 						<div className="relative mb-4 h-60 w-60 text-muted-foreground">
@@ -25,7 +28,7 @@ const VerifyEmailPage = ({searchParams}: PageProps) => {
 						{
 							toEmail ?
 								<p className="text-muted-foreground text-center">
-									We&apos;ve sent a verification link to <span className="font-semibold">{toEmail}</span>.
+									We&apos;ve sent a verification link to <span className="font-semibold">{ toEmail }</span>.
 								</p>
 								:
 								<p className="text-muted-foreground text-center">
@@ -33,7 +36,7 @@ const VerifyEmailPage = ({searchParams}: PageProps) => {
 								</p>
 						}
 					</div>
-				)}
+				) }
 			</div>
 		</div>
 	);
