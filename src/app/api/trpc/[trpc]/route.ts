8a95a2cfbaa0346924ad622ba204b7dd87939d52
Dmitriy Mazurenko <dimazurenko@gmail.com>
2024-03-11 16:07:15 +0300

    update api sign in

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,13 +1,14 @@
-import {fetchRequestHandler} from '@trpc/server/adapters/fetch';
-import {appRouter} from '@/trpc';
+import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
+import { appRouter } from '@/trpc';
 
 const handler = (req: Request) => {
-	fetchRequestHandler({
-		endpoint: '/api/trpc',
-		req,
-		router: appRouter,
-		createContext: () => ({})
-	});
+  fetchRequestHandler({
+    endpoint: '/api/trpc',
+    req,
+    router: appRouter,
+    // @ts-expect-error context already pass from express middleware
+    createContext: () => ({}),
+  });
 };
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,40 @@
 import express from 'express';
 import * as trpcExpress from '@trpc/server/adapters/express';
-import {getPayloadClient} from './get-payload';
-import {nextApp, nextHandler} from './next-utils';
-import {appRouter} from './trpc';
+import { getPayloadClient } from './get-payload';
+import { nextApp, nextHandler } from './next-utils';
+import { appRouter } from './trpc';
+import { inferAsyncReturnType } from '@trpc/server';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
-const createContext = ({req, res}: trpcExpress.CreateExpressContextOptions) => ({
-	req, res
+const createContext = ({
+												 req,
+												 res,
+											 }: trpcExpress.CreateExpressContextOptions) => ({
+	req,
+	res,
 });
+
+export type ExpressContext = inferAsyncReturnType<typeof createContext>;
 
 const start = async () => {
 	const payload = await getPayloadClient({
 		initOptions: {
 			express: app,
 			onInit: async (cms) => {
-				cms.logger.info(`Admin URL ${cms.getAdminURL()}`);
-			}
-		}
+				cms.logger.info(`Admin URL ${ cms.getAdminURL() }`);
+			},
+		},
 	});
 
-	app.use('/api/trpc', trpcExpress.createExpressMiddleware({
-		router: appRouter,
-		createContext
-	}));
+	app.use(
+		'/api/trpc',
+		trpcExpress.createExpressMiddleware({
+			router: appRouter,
+			createContext,
+		})
+	);
 
 	app.use((req, res) => nextHandler(req, res));
 
@@ -32,7 +42,9 @@ const start = async () => {
 		payload.logger.info('Next.js started');
 
 		app.listen(PORT, async () => {
-			payload.logger.info(`Next.js App URL:${process.env.NEXT_PUBLIC_SERVER_URL}`);
+			payload.logger.info(
+				`Next.js App URL:${ process.env.NEXT_PUBLIC_SERVER_URL }`
+			);
 		});
 	});
 };
